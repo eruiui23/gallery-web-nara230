f4d046fdbaac90e24071707e8ca80e22ac3db2b0
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-15 20:45:16 +0700

    fixed CLS

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -1,20 +1,36 @@
+"use client"
 import { Photo } from "@/lib/cloudinary";
 import Image from "next/image";
+import { useState } from "react";
 
 type Props = {
     photo: Photo;
 };
 
 export default function ImageContainer({ photo }: Props) {
+
+    const [isLoading, setIsLoading] = useState(true)
+
     return (
-        <div className="bg-neutral-900 group cursor-pointer">
+        <div
+            style={{ aspectRatio: `${photo.width} / ${photo.height}` }}
+            className="bg-neutral-900 group cursor-pointer relative w-full"
+        >
+
             <Image
                 src={photo.src}
                 alt="Gallery Photo"
                 width={photo.width}
                 height={photo.height}
+                // fill={true}
                 sizes="(max-width: 768px) 50vw, 33vw"
-                className="w-full h-auto object-cover transition-all duration-400 group-hover:brightness-75 group-hover:scale-102"
+                // className=" object-cover transition-all duration-400 group-hover:brightness-75 group-hover:scale-102"
+                className={`
+                                    object-cover transition-all duration-700 ease-in-out
+                                    group-hover:brightness-75 group-hover:scale-105
+                                    ${isLoading ? "opacity-0 blur-sm" : "opacity-100 blur-0"}
+                                `}
+                onLoad={() => setIsLoading(false)}
             />
         </div>
     );
