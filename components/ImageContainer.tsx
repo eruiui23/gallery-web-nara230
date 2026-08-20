71a27e95c172f3461e07af1fbe520968f9c4f8e5
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-20 17:11:01 +0700

    fixed color placeholder not working

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
   photo: Photo;
   preload?: boolean;
   onClick?: () => void;
-  ref?: React.Ref<HTMLDivElement>
+  ref?: React.Ref<HTMLDivElement>;
 };
 
 export default function ImageContainer({
@@ -20,31 +20,31 @@ export default function ImageContainer({
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div
-      style={{
-        aspectRatio: `${photo.width} / ${photo.height}`,
-        backgroundColor: photo.color,
-      }}
-      onClick={onClick}
-      className="bg-neutral-900 group cursor-pointer relative w-full"
-      ref={ref}
-      {...props}
-    >
-      <Image
-        src={photo.src}
-        alt="Gallery Photo"
-        width={photo.width}
-        height={photo.height}
-        sizes="(max-width: 768px) 50vw, 33vw"
-        preload={preload}
-        // className=" object-cover transition-all duration-400 group-hover:brightness-75 group-hover:scale-102"
-        className={`
-                                    object-cover transition-all duration-700 ease-in-out
-                                    group-hover:scale-105
-                                    ${isLoading ? "opacity-0 blur-sm" : "opacity-100 blur-0"}
-                                `}
-        onLoad={() => setIsLoading(false)}
-      />
+    <div ref={ref} {...props} className="w-full">
+      <div
+        style={{
+          aspectRatio: `${photo.width} / ${photo.height}`,
+          backgroundColor: photo.color,
+        }}
+        onClick={onClick}
+        className="bg-neutral-900 group cursor-pointer relative w-full"
+      >
+        <Image
+          src={photo.src}
+          alt="Gallery Photo"
+          width={photo.width}
+          height={photo.height}
+          sizes="(max-width: 768px) 50vw, 33vw"
+          preload={preload}
+          // className=" object-cover transition-all duration-400 group-hover:brightness-75 group-hover:scale-102"
+          className={`
+                                      object-cover transition-all duration-700 ease-in-out
+                                      group-hover:scale-105
+                                      ${isLoading ? "opacity-0 blur-sm" : "opacity-100 blur-0"}
+                                  `}
+          onLoad={() => setIsLoading(false)}
+        />
+      </div>
     </div>
   );
 }
