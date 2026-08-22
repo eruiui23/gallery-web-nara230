0127150b9ce3c0b5383c962f5fca9cf42d6f6101
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-22 09:38:56 +0700

    fix image not appearing when going back and forward fast

--- components/Lightbox.tsx
+++ components/Lightbox.tsx
@@ -19,18 +19,14 @@ export default function Lightbox({
   onClose,
   onNavigate,
 }: Props) {
-
-  const [loadedIndex, setLoadedIndex] = useState<number | null>(null);
-
+  const [loadedImages, setLoadedImages] = useState<number[]>([]);
 
   useEffect(() => {
     if (currentIndex === null) {
       document.body.style.overflow = "auto";
       return;
     }
-
     document.body.style.overflow = "hidden";
-
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Escape") onClose();
       if (e.key === "ArrowLeft" && currentIndex > 0) {
@@ -52,7 +48,7 @@ export default function Lightbox({
   if (typeof document === "undefined") return null;
 
   return createPortal(
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       {currentIndex !== null && (
         <motion.div
           initial={{ opacity: 0 }}
@@ -67,7 +63,16 @@ export default function Lightbox({
             onClick={onClose}
             className="absolute top-6 right-6 text-white z-50 p-2 opacity-70 hover:opacity-100 transition-opacity"
           >
-            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="w-10">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="w-10"
+            >
               <line x1="18" y1="6" x2="6" y2="18"></line>
               <line x1="6" y1="6" x2="18" y2="18"></line>
             </svg>
@@ -79,10 +84,20 @@ export default function Lightbox({
               onClick={(e) => {
                 e.stopPropagation();
                 onNavigate(currentIndex - 1);
+
               }}
               className="hidden sm:block absolute left-0 xl:left-4 text-white z-50 py-10 opacity-70 hover:opacity-100 transition-opacity"
             >
-              <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="w-18">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-18"
+              >
                 <polyline points="15 18 9 12 15 6"></polyline>
               </svg>
             </button>
@@ -97,37 +112,54 @@ export default function Lightbox({
               }}
               className="hidden sm:block absolute right-0 xl:right-4 text-white z-50 py-10 opacity-70 hover:opacity-100 transition-opacity"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="w-18">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-18"
+              >
                 <polyline points="9 18 15 12 9 6"></polyline>
               </svg>
             </button>
           )}
           <AnimatePresence>
-          <motion.div
-            key={currentIndex}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: loadedIndex === currentIndex ? 1 : 0 }}
-            exit={{ opacity: 0, transition: { duration:0.4, ease: "easeOut"} }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
-            className="absolute w-full h-full max-w-7xl max-h-[90vh] mx-10 md:mx-20 flex items-center justify-center px-10 sm:px-20"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <Image
-              src={photos[currentIndex].src}
-              alt="Enlarged gallery photo"
-              width={photos[currentIndex].width}
-              height={photos[currentIndex].height}
-              className="object-contain w-full h-full max-h-[80vh]"
-              unoptimized
-              priority
-              onLoad={() => setLoadedIndex(currentIndex)}
-            />
-          </motion.div>
-        </AnimatePresence>
+            <motion.div
+              key={currentIndex}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: loadedImages.includes(currentIndex) ? 1 : 0 }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 0.4, ease: "easeOut" },
+              }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
+              className="absolute w-full h-full max-w-7xl max-h-[90vh] mx-10 md:mx-20 flex items-center justify-center px-10 sm:px-20"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Image
+                src={photos[currentIndex].src}
+                alt="Enlarged gallery photo"
+                width={photos[currentIndex].width}
+                height={photos[currentIndex].height}
+                className="object-contain w-full h-full max-h-[80vh]"
+                unoptimized
+                priority
+                onLoad={() => {
+                  setLoadedImages((prev) =>
+                    prev.includes(currentIndex)
+                      ? prev
+                      : [...prev, currentIndex],
+                  );
+                }}
+              />
+            </motion.div>
+          </AnimatePresence>
         </motion.div>
-
       )}
     </AnimatePresence>,
-    document.body
+    document.body,
   );
 }
