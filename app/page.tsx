a041e687f8a75151be2d79e0c6f9e2101c38ddb9
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-23 17:09:34 +0700

    adjustment to tab divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,34 @@
 import { EmblaCarousel } from "@/components/EmblaCarousel";
 import ImageWall from "@/components/ImageWall";
+import GalleryTabs from "@/components/GalleryTabs";
 
-function FavDivider() {
-    return (
-        <div className="divider divider-primary divider-start uppercase font-[Arial]">
-            <span className="-mr-2  font-[Gotham]">Fav</span> Fragments
-        </div>
-    );
-}
+const myTabs = [
+  {
+    id: "fragments",
+    label: "Fragments",
+    content: <ImageWall key="uprak" folderName="Nara230/Gallery/Uprak" />,
+  },
+  {
+    id: "memories",
+    label: "Memories",
+    content: (
+      <ImageWall key="classmeet" folderName="Nara230/Gallery/classmeet" />
+    ),
+  },
+];
 
 export default function Home() {
-    return (
-        <div className="flex flex-col gap-5">
-            <div className="h-screen flex flex-col justify-between py-20">
-                <div className="flex flex-1 items-center justify-center overflow-hidden">
-                    <EmblaCarousel />
-                </div>
-            </div>
-            <div className="flex w-7/8 flex-col m-auto">
-                <FavDivider />
-            </div>
-            <ImageWall folderName="Nara230/Gallery/Uprak" />
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="h-screen flex flex-col justify-between py-20">
+        <div className="flex flex-1 items-center justify-center overflow-hidden">
+          <EmblaCarousel />
         </div>
-    );
+      </div>
+      <div className="flex w-7/8 flex-col m-auto">
+        <GalleryTabs tabs={myTabs} />
+      </div>
+      {/*<ImageWall folderName="Nara230/Gallery/Uprak" />*/}
+    </div>
+  );
 }
