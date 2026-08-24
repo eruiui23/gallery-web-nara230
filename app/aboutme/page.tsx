d6f7b9726dbe3c41b34012ed236867ae39f59098
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-24 21:16:47 +0700

    few adjustment

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function AboutMe() {
   return (
     <div className=" flex flex-col justify-center h-screen items-center">
-      <div className="w-100 px-10">
+      <div className="w-125 px-10">
         <Image
           src="https://media.discordapp.net/attachments/754934235051196516/1541331006395777024/image0.jpg?ex=6a8d33dc&is=6a8be25c&hm=3a9dd61ca0a413038a9eebe19f12147ebbd804af709a0dc2571b69252aa7d80e&=&format=webp&width=816&height=1024"
           alt="image of me"
@@ -11,7 +11,8 @@ export default function AboutMe() {
           height={1024}
           sizes="33vw"
         />
-        <h1 className="text-3xl text-center mt-15 font-bold">about me nya belom jadi ...</h1>
+        <h1 className="text-3xl text-center mt-15 font-bold text-nowrap">about me nya belom jadi ...</h1>
+        <p className="text-center mt-5">btw kalo mau takedown foto dm ig @reynr aja y :D</p>
       </div>
     </div>
   );
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -11,7 +11,7 @@ export default async function GalleryPage() {
       content: (
         <div
           key={tab.id}
-          className="max-w-450 px-4 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-items-center"
+          className="max-w-450 min-h-screen px-4 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 content-start "
         >
           {tab.items.map((item) => (
             <Link href={item.href} key={item.href}>
@@ -24,7 +24,7 @@ export default async function GalleryPage() {
   });
 
   return (
-    <div className="mt-30 flex justify-center mx-auto w-7/8">
+    <div className="mt-30 flex justify-center mx-auto w-7/8 pb-10">
       <div className="flex flex-col">
         <GalleryTabs tabs={myTabs} />
       </div>
