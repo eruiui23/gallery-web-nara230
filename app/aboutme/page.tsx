0a033c821132ec35011d21824d4b76578a9d9e91
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-24 13:23:35 +0700

    finished about me... or not?

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function AboutMe() {
-    return (
-        <div>
-            <p className="text-amber-100 text-4xl">
-                Hello, ini adalah halaman About Me
-            </p>
-        </div>
-    );
+  return (
+    <div className=" flex flex-col justify-center h-screen items-center">
+      <div className="w-100 px-10">
+        <Image
+          src="https://media.discordapp.net/attachments/754934235051196516/1541331006395777024/image0.jpg?ex=6a8d33dc&is=6a8be25c&hm=3a9dd61ca0a413038a9eebe19f12147ebbd804af709a0dc2571b69252aa7d80e&=&format=webp&width=816&height=1024"
+          alt="image of me"
+          width={816}
+          height={1024}
+          sizes="33vw"
+        />
+        <h1 className="text-3xl text-center mt-15 font-bold">about me nya belom jadi ...</h1>
+      </div>
+    </div>
+  );
 }
