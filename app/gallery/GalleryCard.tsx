deab7bbd7f28ceef663e126e5f9d6144e35984e1
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-24 13:04:29 +0700

    responsive gallery card and animation

--- app/gallery/GalleryCard.tsx
+++ app/gallery/GalleryCard.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import { motion } from "motion/react";
 
 type Props = {
   title: string;
@@ -6,31 +9,51 @@ type Props = {
 };
 
 export default function GalleryCard({ title, src }: Props) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const callIsLoaded = () => setIsLoaded(true);
+
   return (
-    <div className="w-full bg-white p-2  cursor-pointer group">
-      <div className=" w-full bg-white overflow-hidden">
+    <motion.div
+      className="w-full border-4 border-white"
+      initial={{ opacity: 0  }}
+      animate={{ opacity: 100 }}
+      transition={{ duration: 0.3}}
+    >
+      <div className=" w-full bg-white relative">
         <Image
           src={src}
           alt="test"
-          width={1200}
-          height={800}
-          sizes="(max-width: 768px) 50vw, 33vw"
-          className="group-hover:scale-110 duration-300 object-cover"
+          width={600}
+          height={400}
+          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+          className={`hover:scale-105 duration-300 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+          onLoad={callIsLoaded}
         />
+        <div
+          className={`thinfont absolute bottom-0.5 left-1.5 text-3xl sm:text-xl md:text-xl lg:text-3xl text-balance  text-white font-medium text-shadow-md/30 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+        >
+          {title}
+        </div>
       </div>
-
-      <div className="flex flex-row justify-between thinfont pt-2 px-1 text-xs sm:text-sm md:text-base xl:text-xl text-black font-medium group-hover:text-black/80 duration-300 ">
-        <div className="">{title}</div>
-        <div className="">06/25</div>
-      </div>
-    </div>
+    </motion.div>
   );
 }
 
-// <div className="w-full border-2 border-white">
-//     <div className=" w-full bg-black relative">
-//         <Image src={src} alt="test" width={600} height={400} className="hover:scale-105 duration-300" />
-//         <div className="thinfont absolute bottom-0.5 left-1.5 text-3xl text-white font-medium ">{title}</div>
-//     </div>
+// <div className="w-full bg-white p-2  cursor-pointer group">
+// <div className=" w-full bg-white overflow-hidden">
+//   <Image
+//     src={src}
+//     alt="test"
+//     width={1200}
+//     height={800}
+//     sizes="(max-width: 768px) 50vw, 33vw"
+//     className="group-hover:scale-110 duration-300 object-cover"
+//   />
+// </div>
 
+// <div className="flex flex-row justify-between thinfont pt-2 px-1 text-xs sm:text-sm md:text-base xl:text-xl text-black font-medium group-hover:text-black/80 duration-300 ">
+//   <div className="">{title}</div>
+//   <div className="">06/25</div>
+// </div>
 // </div>
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,32 +2,99 @@ import Image from "next/image";
 import { getImagesFromFolder, Photo } from "@/lib/cloudinary";
 import GalleryCard from "./GalleryCard";
 import Link from "next/link";
+import GalleryTabs from "@/components/GalleryTabs";
+import { div } from "motion/react-client";
+
+// nanti taro key
+const myTabs = [
+  {
+    id: "street",
+    label: "Street",
+    content: (
+
+      <div className="flex flex-col just" key="street">
+        <div className="max-w-350 mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-8 justify-items-center">
+          <Link href="gallery/MalamTahunBaru2024">
+            <GalleryCard
+              title="New Year's Eve 2024"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507388/DSCF5988_web_edltpj.jpg"
+            />
+          </Link>
+        </div>
+      </div>
+
+    ) ,
+  },
+  {
+    id: "highschool",
+    label: "High School",
+    content: (
+      <div className="flex flex-col" key="hs">
+        <div className="max-w-350 mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-8 justify-items-center">
+          <Link href="/gallery/classmeet">
+            <GalleryCard
+              title="Classmeet"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1773988817/DSCF7425_web_uv4cun.jpg"
+            />
+          </Link>
+          <Link href="gallery/Uprak">
+            <GalleryCard
+              title="uprak"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507500/DSCF6732_web_xzbt1h.jpg"
+            />
+          </Link>
+        </div>
+
+      </div>
+    ),
+  },
+  {
+    id: "college",
+    label: "College",
+    content: (
+
+      <div className="flex flex-col" key="street">
+        <div className="max-w-450 px-4 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-8 justify-items-center ">
+          <Link href="gallery/MalamTahunBaru2024">
+            <GalleryCard
+              title="New Year's Eve 2024"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507388/DSCF5988_web_edltpj.jpg"
+            />
+          </Link>
+          <Link href="/gallery/classmeet">
+            <GalleryCard
+              title="Classmeet"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1773988817/DSCF7425_web_uv4cun.jpg"
+            />
+          </Link>
+          <Link href="gallery/Uprak">
+            <GalleryCard
+              title="uprak"
+              src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507500/DSCF6732_web_xzbt1h.jpg"
+            />
+          </Link>
+
+        </div>
+      </div>
+
+    ) ,
+  },
+  // {
+  //   id: "others",
+  //   label: "others",
+  //   content: (
+  //     <div>yaho</div>
+  //   )
+  // },
+];
 
 export default async function GalleryPage() {
-    const photos = await getImagesFromFolder("Nara230/Test");
+  const photos = await getImagesFromFolder("Nara230/Test");
 
-    return (
-        <div className="mt-35 flex justify-center">
-            <div className="max-w-350 px-4 grid grid-cols-2 md:grid-cols-3  gap-8 justify-items-center">
-                <Link href="/gallery/classmeet">
-                    <GalleryCard
-                        title="Classmeet"
-                        src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1773988817/DSCF7425_web_uv4cun.jpg"
-                    />
-                </Link>
-                <Link href="gallery/MalamTahunBaru2024">
-                    <GalleryCard
-                        title="New Years Eve 2024"
-                        src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507388/DSCF5988_web_edltpj.jpg"
-                    />
-                </Link>
-                <Link href="gallery/Uprak">
-                    <GalleryCard
-                        title="uprak"
-                        src="https://res.cloudinary.com/dgvu6ny5a/image/upload/v1786507500/DSCF6732_web_xzbt1h.jpg"
-                    />
-                </Link>
-            </div>
-        </div>
-    );
+
+  return (
+    <div className="mt-30 flex justify-center mx-auto w-7/8">
+        <GalleryTabs tabs={myTabs}/>
+    </div>
+  );
 }
