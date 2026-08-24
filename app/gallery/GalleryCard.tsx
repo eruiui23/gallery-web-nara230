a429a03598506f8ecba1293ed0d17a538ea19973
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-24 21:59:43 +0700

    fix opacity buged

--- app/gallery/GalleryCard.tsx
+++ app/gallery/GalleryCard.tsx
@@ -8,6 +8,38 @@ type Props = {
   src: string;
 };
 
+// export default function GalleryCard({ title, src }: Props) {
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   const callIsLoaded = () => setIsLoaded(true);
+
+//   return (
+//     <motion.div
+//       className="w-full border-4 border-white"
+//       initial={{ opacity: 0  }}
+//       animate={{ opacity: 1 }}
+//       transition={{ duration: 0.3}}
+//     >
+//       <div className=" w-full bg-white relative">
+//         <Image
+//           src={src}
+//           alt="test"
+//           width={600}
+//           height={400}
+//           sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+//           className={`hover:scale-105 duration-300 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+//           onLoad={callIsLoaded}
+//         />
+//         <div
+//           className={`thinfont absolute bottom-0.5 left-1.5 text-3xl sm:text-xl md:text-xl lg:text-4xl text-balance  text-white  text-shadow-md/20 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+//         >
+//           {title}
+//         </div>
+//       </div>
+//     </motion.div>
+//   );
+// }
+
 export default function GalleryCard({ title, src }: Props) {
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -15,45 +47,28 @@ export default function GalleryCard({ title, src }: Props) {
 
   return (
     <motion.div
-      className="w-full border-4 border-white"
-      initial={{ opacity: 0  }}
+      className="w-full bg-white p-2  cursor-pointer group"
+      initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.3}}
+      transition={{ duration: 0.3 }}
     >
-      <div className=" w-full bg-white relative">
+      <div className=" w-full bg-white overflow-hidden">
         <Image
           src={src}
           alt="test"
-          width={600}
-          height={400}
-          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
-          className={`hover:scale-105 duration-300 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+          width={1200}
+          height={800}
+          sizes="(max-width: 768px) 50vw, 33vw"
+          className={`group-hover:scale-105 group-hover:opacity-80 duration-300 object-cover ${isLoaded ? "opacity-100" : "opacity-0"}`}
           onLoad={callIsLoaded}
         />
-        <div
-          className={`thinfont absolute bottom-0.5 left-1.5 text-3xl sm:text-xl md:text-xl lg:text-4xl text-balance  text-white font-medium text-shadow-md/30 ${isLoaded ? "opacity-100" : "opacity-0"}`}
-        >
+      </div>
+
+      <div className="flex flex-row justify-between thinfont pt-2 px-1 text-xl sm:text-sm md:text-base xl:text-xl text-black font-medium group-hover:text-black/80 duration-300 ">
+        <div className="group-hover:underline underline-offset-4 text-nowrap">
           {title}
         </div>
       </div>
     </motion.div>
   );
 }
-
-// <div className="w-full bg-white p-2  cursor-pointer group">
-// <div className=" w-full bg-white overflow-hidden">
-//   <Image
-//     src={src}
-//     alt="test"
-//     width={1200}
-//     height={800}
-//     sizes="(max-width: 768px) 50vw, 33vw"
-//     className="group-hover:scale-110 duration-300 object-cover"
-//   />
-// </div>
-
-// <div className="flex flex-row justify-between thinfont pt-2 px-1 text-xs sm:text-sm md:text-base xl:text-xl text-black font-medium group-hover:text-black/80 duration-300 ">
-//   <div className="">{title}</div>
-//   <div className="">06/25</div>
-// </div>
-// </div>
